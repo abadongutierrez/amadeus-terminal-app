9a6234c1c6a5d62a6acf60a469bf011cbcd3f732
gmotzespina <developer@bitcodelabs.com.mx>
2018-08-29 19:29:20 -0500

    Add spring application

---
+++ src/main/java/com/nearsfot/amadeus/amadeusterminalapp/AmadeusTerminalAppApplication.java
@@ -0,0 +1,13 @@
+package com.nearsfot.amadeus.amadeusterminalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusTerminalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusTerminalAppApplication.class, args);
+
+	}
+}
